f2123477628f97df3d0e1c26f8091a7a23ccf8ef
Surav Shrestha <surav.shrestha@outlook.com>
2024-02-25 21:09:20 +0545

    change email -> username

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -17,7 +17,7 @@ const LoginForm = () => {
 
   const notification = useAppSelector((state) => state.notification);
 
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
@@ -28,7 +28,7 @@ const LoginForm = () => {
       token: "1234567890",
       firstName: "John",
       lastName: "Doe",
-      email,
+      username,
     };
 
     const notification: INotification = {
@@ -40,7 +40,7 @@ const LoginForm = () => {
     dispatch(setUser(user));
     dispatch(setNotification(notification));
 
-    setEmail("");
+    setUsername("");
     setPassword("");
 
     navigate("/");
@@ -78,20 +78,20 @@ const LoginForm = () => {
         <form onSubmit={handleLogin}>
           <div className="relative z-0 w-full mb-5 group">
             <input
-              type="email"
-              name="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              type="text"
+              name="username"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
               placeholder=" "
               required
             />
             <label
-              htmlFor="email"
+              htmlFor="username"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
-              Email address
+              Username
             </label>
           </div>
 
