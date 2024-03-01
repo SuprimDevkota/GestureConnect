4e79a4788d1e4fd6fb0ee6c023bc856c7099e588
Surav Shrestha <surav.shrestha@outlook.com>
2024-03-01 13:39:30 +0545

    configure client prod settings for communication with backend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api/": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  process.env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    server: {
+      proxy: {
+        "/api/": {
+          target: process.env.DEV
+            ? "http://localhost:8000"
+            : "https://gestureconnect-aqiz.onrender.com",
+          changeOrigin: true,
+        },
       },
     },
-  },
-  plugins: [react()],
+    plugins: [react()],
+  };
 });
