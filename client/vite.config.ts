b9ead6035ff6cc1c7d121635723fbe93f6389301
Surav Shrestha <surav.shrestha@outlook.com>
2024-03-02 10:55:49 +0545

    update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,23 +1,15 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  process.env = loadEnv(mode, process.cwd(), "");
-
-  console.log("process.env", process.env)
-
-  return {
-    server: {
-      proxy: {
-        "/api/": {
-          target: process.env.DEV
-            ? "http://localhost:8000"
-            : "https://gestureconnect-aqiz.onrender.com",
-          changeOrigin: true,
-        },
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api/": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
       },
     },
-    plugins: [react()],
-  };
+  },
+  plugins: [react()],
 });
