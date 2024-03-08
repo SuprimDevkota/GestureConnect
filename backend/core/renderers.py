725d4e9d5d3a619d5605d19d0a44ec52f2c9470d
Surav Shrestha <surav.shrestha@outlook.com>
2024-03-08 09:50:30 +0545

    fix: response casing from api

--- backend/core/renderers.py
+++ backend/core/renderers.py
@@ -1,13 +1,15 @@
-from rest_framework import renderers
+from djangorestframework_camel_case.render import CamelCaseJSONRenderer
 import json
 
-class UserRenderer(renderers.JSONRenderer):
-  charset='utf-8'
-  def render(self, data, accepted_media_type=None, renderer_context=None):
-    response = ''
-    if 'ErrorDetail' in str(data):
-      response = json.dumps({'errors':data})
-    else:
-      response = json.dumps(data)
-    
-    return response
+
+class UserRenderer(CamelCaseJSONRenderer):
+    charset = 'utf-8'
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        # Call parent class's render method to convert to camelCase
+        response = super().render(data, accepted_media_type, renderer_context)
+
+        if 'ErrorDetail' in str(data):
+            response = json.dumps({'errors': data})
+
+        return response
